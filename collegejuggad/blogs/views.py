6331408116971d3f9cd5a4b7107b2b3dbcc7a4ca
Nitesh639 <nkush9598@gmail.com>
2022-09-05 01:16:17 +0530

    index_update

--- collegejuggad/blogs/views.py
+++ collegejuggad/blogs/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'y': [1, 2, 3]})
